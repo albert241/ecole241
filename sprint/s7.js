d32f419b27379aad1bf13d4d9f9a184705d83c19
albert241 <albertsisso6@gmail.com>
2019-02-18 15:33:41 +0100

    ajout de s7.js

--- sprint/s7.js
+++ sprint/s7.js
@@ -1,6 +1,4 @@
-let nombre = Number(prompt("Entrez un nombre positif"));
-if (nombre < 0) {
-  console.log("Entrez un nombre positif s'il vous plait");
-} else if (nombre > 0) {
-  console.log("J'adore les nombres positifs");
+let index = 1;
+while (index > 0) {
+index = prompt("Entrez un nombre");
 }
