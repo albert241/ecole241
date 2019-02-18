2508455e9bb1db518f300ea2cd45c0b1e384d0bf
albert241 <albertsisso6@gmail.com>
2019-02-18 14:16:30 +0100

    ajout de s5.js

---
+++ sprint/s5.js
@@ -0,0 +1,12 @@
+function nombre() {
+  Number(prompt("Entrer un nombre"));
+  if(nombre > 1) {
+    console.log(2);
+  };
+  Number(prompt("Entrer un nombre"));
+  if(nombre < 2) {
+    console.log(1)
+  }
+}
+
+nombre()
