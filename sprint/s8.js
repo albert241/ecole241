1151adc37c24dac0f2ea74d9519c00de2470b51d
albert241 <albertsisso6@gmail.com>
2019-02-20 10:20:15 +0100

    ajout de s8

---
+++ sprint/s8.js
@@ -0,0 +1,6 @@
+let nombre;
+do{
+  nombre = + prompt("Entrez un nombre svp: ");
+}while(Number .isNaN(nombre));
+for(i = 1; i <= 10; i++) {
+  console.log(`${nombre} x ${i} = ${nombre * i}`)}
---
+++ sprint/s9.js
@@ -0,0 +1,10 @@
+function nbreMax() {
+  nbre1 = Number(prompt("Entrez un nombre 1"));
+  nbre2 = Number(prompt("Entrez un nombre 2"));
+  nbre3 = Number(prompt("Entrez un nombre 3"));
+  nbre3 = Number(prompt("Entrez un nombre 4"));
+  nbre3 = Number(prompt("Entrez un nombre 5"));
+
+}
+
+nbreMax()
