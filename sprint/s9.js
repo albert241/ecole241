31325507683345561d51b81d0acb4c2041c26bb9
albert241 <albertsisso6@gmail.com>
2019-02-25 16:30:48 +0100

    correction s5.js

--- sprint/s9.js
+++ sprint/s9.js
@@ -1,9 +1,9 @@
-let max, nbre;
+let max, nombre;
 max = + prompt("Entrez le nombre 1 svp:");
 for (let i = 1; i <= 5; i++) {
-    nbre = + prompt(`Entrez le nombre ${i}`);
-    if (nbre > max) {
-        max = nbre;
+    nombre = + prompt(`Entrez le nombre ${i}`);
+    if (nombre > max) {
+        max = nombre;
     }
 }
 console.log(`Le plus grand est : ${max}`);
