7d9258bc7694f0c11b1f30cff6fd5bccbef82c4b
albert241 <albertsisso6@gmail.com>
2019-03-26 14:47:05 +0100

    Ajout act2.php dans sprint/php

---
+++ sprint/php/act2.php
@@ -0,0 +1,10 @@
+<?php
+    $nom_perso = "SISSO";
+    $age_perso = 28;
+    $sexe_perso = "masculin";
+    $perso_male = true;
+    $taille_perso = 1.70;
+    $sans_valeur = NULL;
+    echo "je m'appelle " . $nom_perso . " j'ai " . $age_perso . " ans et je mesure" . $taille_perso . " m. <br />Je suis de sexe " . $sexe_perso;
+    echo " j'affiche le type bool " . $perso_male;
+?>
